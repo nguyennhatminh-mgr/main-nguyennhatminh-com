6f41a9999f9f5fa87b0e00443590ee6c6546594f
Minh Nguyen <nhatminhuseforgithub@gmail.com>
2024-04-11 17:00:22 +0700

    feat: Update meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,10 @@ import CardScreensImage from "@/images/card_screens.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Connect people to the future",
+  title: "Connect people to the future | Minh's group",
   description: "Help you build anything to make your ideas come true",
   openGraph: {
-    title: "Connect people to the future",
+    title: "Connect people to the future | Minh's group",
     description: "Help you build anything to make your ideas come true",
     images: CardScreensImage.src,
   },
